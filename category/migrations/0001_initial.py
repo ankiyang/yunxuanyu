2a21fc9f9b6b4eac68d8c16070f313e255986563
ankiyang <398753283@qq.com>
2016-09-20 11:27:53 +0800

    add category

---
+++ category/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=30, verbose_name='\u540d\u5b57')),
+                ('desc', models.CharField(max_length=150, verbose_name='\u63cf\u8ff0')),
+                ('create_timestamp', models.DateTimeField(auto_now_add=True)),
+                ('manager', models.ForeignKey(verbose_name=b'\xe4\xbd\x9c\xe8\x80\x85', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name': '\u5206\u7c7b',
+                'verbose_name_plural': '\u5206\u7c7b',
+            },
+        ),
+    ]
